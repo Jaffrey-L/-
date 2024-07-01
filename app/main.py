737a89bf36826e13613e96ce318400a7d1aeb14f
eric <deluxebear@gmail.com>
2024-07-01 10:35:02 +0800

    修正领星get请求时提示token不正确问题

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 import json
 import logging
+from typing import List, Dict, Any, Optional
 
 import httpx
 from fastapi import FastAPI, Request, HTTPException
+from fastapi.params import Body
+from k3cloud_webapi_sdk.main import K3CloudApiSdk
 from pydantic import BaseModel
+from pymongo import MongoClient
 
 from app.AsyncPostgres import AsyncPostgres
 from app.ihr import login, fetch_all_pages
@@ -209,7 +213,7 @@ class TableNameRequest(BaseModel):
 
 
 """
-清楚postgresql中表里面的内容
+清除postgresql中表里面的内容
 """
 
 
@@ -229,6 +233,103 @@ async def clear_table(request: TableNameRequest):
         except Exception as e:
             logger.error(f"Failed to clear table {table_name}: {e}")
             raise HTTPException(status_code=500, detail="Failed to clear the table")
+
+
+k3api_sdk = K3CloudApiSdk()
+k3api_sdk.InitConfig("65790e1ca8a581", "kd", "267507_R7cJ3xiIUvAY4awJQ7Rs5z9H3sR+QoOu",
+                     "a16e530ad15546dfa318a8f950025ff5")
+
+
+@app.post("/k3/bill_query")
+async def k3_bill_query(request: Request):
+    body = await request.json()
+    response = k3api_sdk.BillQuery(body)
+    return json.loads(response)
+
+
+@app.post("/k3/sys_report_query")
+async def k3_sys_report_query(request: Request):
+    body = await request.json()
+    form_id = body.get("formId")
+    data = body.get("data")
+    response = k3api_sdk.getSysReportData(form_id, data)
+    return json.loads(response)
+
+
+@app.post("/k3/query_bussiness_info")
+async def k3_query_bussiness_info(request: Request):
+    body = await request.json()
+
+    metadata = k3api_sdk.QueryBusinessInfo(body)
+    metadata = json.loads(metadata)
+    # 初始化一个列表来存储字段信息
+    fields_info = {}
+
+    try:
+        # 检查'Entrys'键是否存在于元数据中
+        entries = metadata['Result']['NeedReturnData']['Entrys']
+    except KeyError:
+        print("The expected data structure is missing in the metadata.")
+        return fields_info  # 返回空列表
+
+    # 遍历每个条目
+    for entry in entries:
+        try:
+            # 获取每个条目中的'Fields'数组
+            fields = entry['Fields']
+        except KeyError:
+            # 如果条目中没有'Fields'键，继续处理下一个条目
+            continue
+
+        # 遍历每个字段
+        for field in fields:
+            try:
+                # 提取字段名和字段名称
+                field_name = field['FieldName']
+                # 假设语言代码2052对应的名称
+                human_readable_name = next((name['Value'] for name in field['Name'] if name['Key'] == 2052), None)
+                # 确保字段名和字段名称都存在
+                if field_name and human_readable_name:
+                    fields_info.update({field_name: human_readable_name})
+            except KeyError:
+                # 如果字段缺失必要的键，记录这一异常情况并继续
+                print(f"Missing necessary information in field: {field}")
+                continue
+    keys_string = ', '.join(f"{key}" for key in fields_info.keys())
+    fields_info['keys_string'] = keys_string
+    return fields_info
+
+
+client = MongoClient("mongodb://192.168.1.181:27017/")
+db = client.my_database  # 替换为你的数据库名
+
+
+class QueryParams(BaseModel):
+    view_name: str
+    conditions: Dict[str, Dict[str, Any]]
+    skip: Optional[int] = 0
+    limit: Optional[int] = 10
+
+
+@app.post("/mongodb/view/", response_model=List[Dict])
+async def get_items(query_params: QueryParams = Body(...)):
+    # 根据视图名获取集合
+    collection = db[query_params.view_name]
+
+    # 构建查询条件
+    query = query_params.conditions
+
+    # 查询并分页
+    items = collection.find(query).skip(query_params.skip).limit(query_params.limit)
+
+    # 转换为列表并返回
+    result = []
+    for item in items:
+        item["_id"] = str(item["_id"])  # 将 ObjectId 转换为字符串
+        item["id"] = item["_id"]
+        result.append(item)
+
+    return result
 
 
 if __name__ == "__main__":
